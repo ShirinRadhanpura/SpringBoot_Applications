7f1b3ce88b9f92b68d1711ad9e416551a0a700a5
ShirinRadhanpura <shirin.radhanpura@gmail.com>
2019-05-08 12:08:39 +0530

    Web application using SpringBoot and servlet

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SpringBoot_Applications
+
+#How to run
+run Application and 
+use url on browser :http://localhost:8080/helloworld 
---
+++ src/main/java/upgrad/HelloWorldServlet.java
@@ -0,0 +1,31 @@
+package upgrad;
+
+import jdk.nashorn.internal.objects.Global;
+import org.springframework.http.HttpRequest;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/helloworld")
+public class HelloWorldServlet extends HttpServlet {
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter printWriter=response.getWriter();
+        printWriter.print("<!Doctype html>");
+        printWriter.print("<html>");
+        printWriter.print("<head>");
+        printWriter.print("<title>Hello World Servlet!</title>");
+        printWriter.print("</head>");
+        printWriter.print("<body>");
+        printWriter.print("<p>Hello World123!</p>");
+        printWriter.print("</body>");
+        printWriter.print("</html>");
+        printWriter.close();
+
+    }
+
+}
