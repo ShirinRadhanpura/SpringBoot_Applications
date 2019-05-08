54b36298d2357477e104d0c1180353adc99b57fe
ShirinRadhanpura <shirin.radhanpura@gmail.com>
2019-05-08 17:02:03 +0530

    Web application using Spring MVC

--- src/main/java/upgrad/HelloWorldApplication.java
+++ src/main/java/upgrad/HelloWorldApplication.java
@@ -3,9 +3,11 @@ package upgrad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
+/*uncomment @ServletComponentScan if want to create web application using servlet*/
 @SpringBootApplication
-@ServletComponentScan
+//@ServletComponentScan
 public class HelloWorldApplication {
 
     public  static void main(String args[]){
